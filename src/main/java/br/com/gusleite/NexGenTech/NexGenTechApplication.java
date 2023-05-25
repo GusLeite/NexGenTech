1f572021dc9efaa4dec68ee075a389d24769eff8
Gustavo <liliagustavo@hotmail.com>
2023-05-25 11:22:23 -0300

    Project Create

---
+++ src/main/java/br/com/gusleite/NexGenTech/NexGenTechApplication.java
@@ -0,0 +1,13 @@
+package br.com.gusleite.NexGenTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexGenTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexGenTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gusleite/NexGenTech/NexGenTechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gusleite.NexGenTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexGenTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
